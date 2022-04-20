89c3804583e2b27fc029ce9686b891c7178e44ab
Gonzalo Martinez <87735772+gemadev@users.noreply.github.com>
2022-04-19 22:10:01 -0300

    Update index.js

--- clase 1/index.js
+++ clase 1/index.js
@@ -2,7 +2,7 @@ const num = parseInt(prompt("INGRESE UN NUMERO POSITIVO: "));
 let esPrimo = true;
 
 if (num === 1) {
-    document.write("1 NO ES UN NUMERO PRIMO");
+    document.write("NO ES UN NUMERO PRIMO");
 }else if (num > 1) {
     for (let i = 2; i < num; i++) {
         if (num % i == 0) {
